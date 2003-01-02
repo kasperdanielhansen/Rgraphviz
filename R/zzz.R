ed742c14bb02a7d6521965d314c246f8fd2997e6
Jeff Gentry <jgentry@jimmy.harvard.edu>
2003-01-02 21:05:02 +0000

    added new Ragraph class
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rgraphviz@2872 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,15 @@
 .First.lib <- function(lib, pkg, where) {
+    if (missing(where)) {
+        where <- match(paste("package:", pkgname, sep=""), search())
+        if(is.na(where)) {
+            warning(paste("Not a package name: ",pkgname))
+            return()
+        }
+        where <- pos.to.env(where)
+    }
+
     require(graph)
     library.dynam( "Rgraphviz", pkg, lib )
     .Call("Rgraphviz_init")
+    .initRagraph(where)
 }
