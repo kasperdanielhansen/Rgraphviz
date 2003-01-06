f5961e6c445307780c4075fc999ecd3b6b4cfd5a
Jeff Gentry <jgentry@jimmy.harvard.edu>
2003-01-06 19:23:09 +0000

    new class code
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rgraphviz@2908 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/Ragraph.R
+++ R/Ragraph.R
@@ -1,10 +1,12 @@
 .initRagraph <- function(where) {
     setGeneric("Ragraph", function(object)
                standardGeneric("Ragraph"), where=where)
-    ## layout shoudl be changed to a logical
-    setClass("Ragraph", representation(agraph="externalptr",
-                                       layout="integer"))
 
+    setClass("Ragraph", representation(agraph="externalptr",
+                                       laidout="logical",
+                                       nodeLocs="matrix"
+                                       )
+             )
 
     if (is.null(getGeneric("agraph")))
         setGeneric("agraph", function(object)
@@ -12,9 +14,25 @@
     setMethod("agraph", "Ragraph", function(object)
               object@agraph, where=where)
 
-    if (is.null(getGeneric("layout")))
-        setGeneric("layout", function(object)
-                   standardGeneric("layout"), where=where)
-    setMethod("layout", "Ragraph", function(object)
-              object@layout, where=where)
-}
+    if (is.null(getGeneric("laidout")))
+        setGeneric("laidout", function(object)
+                   standardGeneric("laidout"), where=where)
+    setMethod("laidout", "Ragraph", function(object)
+              object@laidout, where=where)
+
+    if (is.null(getGeneric("nodeLocs")))
+        setGeneric("nodeLocs", function(object)
+                   standardGeneric("nodeLocs"), where=where)
+    setMethod("nodeLocs", "Ragraph", function(object)
+              object@nodeLocs, where=where)
+
+    setMethod("show", "Ragraph", function(object) {
+        cat(paste("A graph with",nrow(nodeLocs(object)),
+                  "nodes.\nThis graph has "))
+        if (laidout(object))
+            cat("already")
+        else
+            cat("has not")
+        cat(" been laid out using GraphViz\n")
+    }, where=where)
+
