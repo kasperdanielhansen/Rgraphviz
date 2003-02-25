874d5ff24b07b3e094d1b3524e760e64264efea0
jgentry <jgentry@bc3139a8-67e5-0310-9ffc-ced21a209358>
2003-02-25 16:26:46 +0000

    fixes to handle new changes from Robert
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rgraphviz@3408 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/common.h
+++ src/common.h
@@ -31,7 +31,7 @@ SEXP Rgraphviz_graph2ps(SEXP, SEXP);
 SEXP Rgraphviz_doDotLayout(SEXP);
 SEXP getBoundBox(Agraph_t *);
 SEXP getEdgeLocs(Agraph_t *,int);
-SEXP Rgraphviz_agopen(SEXP, SEXP, SEXP, SEXP);
+SEXP Rgraphviz_agopen(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 SEXP getNodeLayouts(Agraph_t *);
 Agraph_t *dotLayout(Agraph_t *);
 #endif
