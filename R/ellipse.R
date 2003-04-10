e12e029dc4635e9071587f2f6aedac71f2ce15ef
Jeff Gentry <jgentry@jimmy.harvard.edu>
2003-04-10 18:06:36 +0000

    added ellipse drawing code
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rgraphviz@3976 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/ellipse.R
@@ -0,0 +1,31 @@
+### Written by Sundar Dorai-Raj, sundar.dorai-raj@pdf.com
+
+# begin ellipse
+ellipse <- function(x,y,
+                    width,height=width,theta=2*pi,
+                    npoints=100,plot=T) {
+  # x = x coordinate of center
+  # y = y coordinate of center
+  # width = length of major axis
+  # height = length of minor axis
+  # theta = rotation
+  # npoints = number of points to send to polygon
+  # plot = if TRUE, add to current device
+  # = if FALSE, return list of components
+  a <- width/2
+  b <- height/2
+  xcoord <- seq(-a,a,length=npoints)
+  ycoord.neg <- sqrt(b^2*(1-(xcoord)^2/a^2))
+  ycoord.pos <- -sqrt(b^2*(1-(xcoord)^2/a^2))
+  xx <- c(xcoord,xcoord[npoints:1])
+  yy <- c(ycoord.neg,ycoord.pos)
+  x.theta <- xx*cos(2*pi-theta)+yy*sin(2*pi-theta)+x
+  y.theta <- yy*cos(2*pi-theta)-xx*sin(2*pi-theta)+y
+  if(plot)
+    invisible(polygon(x.theta,y.theta,density=0))
+  else
+    invisible(list(coords=data.frame(x=x.theta,y=y.theta),
+                   center=c(x,y),
+                   theta=theta))
+}
+# end ellipse 
