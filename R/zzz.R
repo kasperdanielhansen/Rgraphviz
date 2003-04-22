5b25de923dea3d72ddc2f0cf8b0504c5689a738d
jgentry <jgentry@bc3139a8-67e5-0310-9ffc-ced21a209358>
2003-04-22 18:52:07 +0000

    added image saving
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rgraphviz@4068 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,7 @@
 .First.lib <- function(lib, pkg, where) {
-    if (missing(where)) {
-        where <- match(paste("package:", pkg, sep=""), search())
-        if(is.na(where)) {
-            warning(paste("Not a package name: ",pkg))
-            return()
-        }
-        where <- pos.to.env(where)
-    }
-
     library.dynam( "Rgraphviz", pkg, lib )
     .Call("Rgraphviz_init")
 
     require(graph) || stop ("Rgraphviz requires package graph.")
-
-    .initRgraphvizMethods(where)
-    .initGraphPlotClasses(where)
 }
 
