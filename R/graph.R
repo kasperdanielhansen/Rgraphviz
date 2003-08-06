88393b37cc67d503b3fd91df13e5e3b543a94265
Jeff Gentry <jgentry@jimmy.harvard.edu>
2003-08-06 19:21:03 +0000

    removed clusterPlot - now built into plot
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rgraphviz@5257 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/graph.R
+++ R/graph.R
@@ -1,149 +1,5 @@
 ### Methods for the graph classes in package graph
 
-
-clusterPlot <- function(x, y, ..., nodeLabels, centerNode, nodeShape,
-                     defNodeCol=par("bg"), nodeCols=character(),
-                     defTextCol=par("fg"), textCols=character(),
-                     defEdgeCol=par("col"),edgeCols=list(),
-                     rankDir, fixedSize=TRUE, subGList){
-
-
-    if (!validGraph(x))
-        stop("The graph to be plotted is not a valid graph structure")
-
-    if (missing(y))
-        y <- "dot"
-
-    ## Get edgemode of graph
-    edgeMode <- edgemode(x)
-    agKind <- switch(edgeMode,
-                     "undirected"="AGRAPH",
-                     "directed"="AGDIGRAPH",
-                     "AGRAPH")
-    nodes <- nodes(x)
-    edges <- edges(x)
-    if(missing(nodeLabels))
-        nodeLabels <- nodes
-    else
-        if (length(nodeLabels) != length(nodes))
-            stop(paste("nodeLabels must be the same",
-                       "length as the number of nodes"))
-
-    ## Generate an attrs list (currently just
-    ## center)
-    attrs <- vector(length=3,mode="list")
-    names(attrs) <- c("graph","node","edge")
-
-    ## First make sure that center exists in
-    ## nodeLabels
-    if (!missing(centerNode))
-        attrs$graph$center <- checkCenterNode(centerNode, nodeLabels)
-
-    ## Check node shapes
-    if (missing(nodeShape))
-        attrs$node$shape <- "circle"
-    else
-        attrs$node$shape <- checkNodeShape(nodeShape)
-
-    ## If this is a dot layout, check the rankdir
-    if (y == "dot") {
-        if (missing(rankDir))
-            attrs$graph$rankdir <- "TB"
-        else
-            attrs$graph$rankdir <- checkRankDir(rankDir)
-    }
-
-    ## Setup the node and text color vectors
-    nC <- getCols(defNodeCol, nodeCols, nodes)
-    tC <- getCols(defTextCol, textCols, nodes)
-
-    nL <- nodeLabels
-    if (fixedSize)
-        nL <- rep(nL[match(max(nchar(nL)), nchar(nL))],
-                  length(nodes))
-
-    ## !!!!!!!!!!!!!!!!!!!
-
-    ## Get the list
-    if (missing(subGList))
-        subGList <- list()
-
-    ## !!!!!!!!!!!!!!!!!!
-
-    g <- agopen(x, "ABC", nL, agKind,  layout=TRUE,
-                layoutType=y, attrs=attrs, subGList)
-
-
-    foo <- testPlot(x,g,nodeLabels, attrs, edgeCols, defEdgeCol,
-                    nC, tC, ...)
-    invisible(foo)
-
-}
-
-
-testPlot <- function(x, g, nodeLabels, attrs, edgeCols,
-                     defEdgeCol, nC, tC, ...) {
-
-    if (length(nodeLabels) > 0) {
-        nodeLocs <- getNodeLocs(g)
-
-        nodeX <- nodeLocs[["x"]]
-        nodeY <- nodeLocs[["y"]]
-
-        ## Get the radii of the nodes.  For now we're just
-        ## implementing circles and ellipses
-        rad <- unlist(lapply(nodes(g), getNodeRW))
-        RWidths <- rad
-        heights <- unlist(lapply(nodes(g), getNodeHeight))
-
-        ## Get the upper right X,Y point of the bounding
-        ## box for the graph
-        ur <- upRight(boundBox(g))
-
-        ## Set up the plot region, plot the edges, then
-        ## nodes and finally the node labels
-        opar <- par(pty="s", oma=c(0,0,0,0),
-                    mai=c(0,0,0,0))
-        on.exit(par=opar, add=TRUE)
-        plot(NA,NA,xlim=c(0,getX(ur)), ylim=c(0,getY(ur)),
-             type="n",main=NULL,xlab="",ylab="",xaxt="n",
-             yaxt="n",bty="n",...)
-
-        rad <- switch(attrs$node$shape,
-                      circle=drawCircleNodes(nodeX, nodeY, ur,
-                      rad, nC),
-                      ellipse=drawEllipseNodes(nodeX, nodeY,
-                      rad*2, heights, nC)
-                      )
-
-
-        ## Plot the edges
-        q <- lapply(AgEdge(g), function(x, edgeCols,
-                                        defEdgeCol, rad) {
-            ## See if there's a specified edgeCol for this
-            if (!is(x,"AgEdge"))
-                stop(paste("Class:",class("AgEdge")))
-            tail <- tail(x)
-            head <- head(x)
-            col <- as.character(edgeCols[[tail]][[head]])
-            if (length(col)==0)
-                col <- defEdgeCol
-            lines(x, col=col, len=(rad / 3))
-        }, edgeCols, defEdgeCol, rad)
-
-        text(nodeX,nodeY, nodeLabels, col=tC)
-                  }
-    else {
-        stop("No nodes in graph")
-    }
-
-    invisible(list(nodeLocs=nodeLocs,
-                   nodeHeights=heights,
-                   nodeRwidths=RWidths,
-                   edges=AgEdge(g),
-                   nodeLabels=nodeLabels))
-}
-
 if (is.null(getGeneric("graph2graphviz")))
     setGeneric("graph2graphviz", function(object,...)
                standardGeneric("graph2graphviz"))
@@ -175,7 +31,7 @@ setMethod("graph2graphviz", "graphNEL", function(object) {
                        defNodeCol=par("bg"), nodeCols=character(),
                        defTextCol=par("fg"), textCols=character(),
                        defEdgeCol=par("col"),edgeCols=list(),
-                       rankDir, fixedSize=TRUE){
+                       rankDir, fixedSize=TRUE, subGList){
                   if (!validGraph(x))
                   stop("The graph to be plotted is not a valid graph structure")
 
@@ -230,8 +86,16 @@ setMethod("graph2graphviz", "graphNEL", function(object) {
                       nL <- rep(nL[match(max(nchar(nL)), nchar(nL))],
                                 length(nodes))
 
+                  ## !!!!!!!!!!!!!!!!!!!
+
+                  ## Get the list
+                  if (missing(subGList))
+                      subGList <- list()
+
+                  ## !!!!!!!!!!!!!!!!!!
+
                   g <- agopen(x, "ABC", nL, agKind,  layout=TRUE,
-                  layoutType=y, attrs=attrs)
+                  layoutType=y, attrs=attrs, subGList)
 
                   if (length(nodeLabels) > 0) {
                       nodeLocs <- getNodeLocs(g)
