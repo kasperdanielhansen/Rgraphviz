54dbe797a4fbfa5a2fc5e5ac798926454270f28e
Jeff Gentry <jgentry@jimmy.harvard.edu>
2003-12-09 21:03:44 +0000

    changed the way pNode gets initialized
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rgraphviz@6931 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plot.R
+++ R/plot.R
@@ -3,18 +3,54 @@
 
 ##some classes for plotting values of nodes and edges
 
-setClass("pNode", representation( name="character",
+setClass("pNode", representation(name="character",
+                                 label="character",
                                  shape="character",
                                  color="character"
                                  ),
-         prototype=list(name="", shape="circle", color="white")
          )
+
+## Use initialize and not prototype as label's default
+## should make it the same as name
+setMethod("initialize", "pNode", function(.Object, name = "",
+                                          label = name,
+                                          shape="circle", color="white") {
+    .Object@name <- name
+    .Object@label <- label
+    .Object@shape <- shape
+    .Object@color <- color
+    .Object
+})
+
+if (is.null(getGeneric("name")))
+    setGeneric("name", function(object)
+               standardGeneric("name"))
+setMethod("name", "pNode", function(object)
+          object@name)
+
+if (is.null(getGeneric("label")))
+    setGeneric("label", function(object)
+               standardGeneric("label"))
+setMethod("label", "pNode", function(object)
+          object@label)
+
+if (is.null(getGeneric("shape")))
+    setGeneric("shape", function(object)
+               standardGeneric("shape"))
+setMethod("shape", "pNode", function(object)
+          object@shape)
+
+if (is.null(getGeneric("color")))
+    setGeneric("color", function(object)
+               standardGeneric("color"))
+setMethod("color", "pNode", function(object)
+          object@color)
 
 
 ##end type can be arrow or -| or none
 ##which can be from, to or both
-setClass("pEdge", representation(from="integer",
-                                 to="integer",
+setClass("pEdge", representation(from="character",
+                                 to="character",
                                  style="character",
                                  color="character",
                                  label="character",
@@ -26,6 +62,12 @@ setClass("pEdge", representation(from="integer",
                                  headport="character",
                                  tailclip="logical",
                                  weight="numeric"
-                                 )
+                                 ),
+         prototype=list(label="")
 )
 
+if (is.null(getGeneric("label")))
+    setGeneric("label", function(object)
+               standardGeneric("label"))
+setMethod("label", "pEdge", function(object)
+          object@label)
