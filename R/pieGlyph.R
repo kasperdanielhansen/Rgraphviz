cd4efb53d3de1ae7433a812a12d9dd4955522524
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-01-30 16:15:35 +0000

    initial changes
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rgraphviz@7355 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/pieGlyph.R
@@ -0,0 +1,31 @@
+pieGlyph <- function (x, xpos, ypos, labels = names(x), edges = 200,
+    radius = 0.8, density = NULL, angle = 45, col = NULL, border =
+    NULL, lty = NULL, main = NULL, ...)
+{
+    if (!is.numeric(x) || any(is.na(x) | x <= 0))
+        stop("pie: `x' values must be positive.")
+    if (is.null(labels))
+        labels <- as.character(1:length(x))
+    x <- c(0, cumsum(x)/sum(x))
+    dx <- diff(x)
+    nx <- length(dx)
+    if (is.null(col))
+        col <- if (is.null(density))
+            c("white", "lightblue", "mistyrose", "lightcyan",
+                "lavender", "cornsilk")
+        else par("fg")
+    col <- rep(col, length.out = nx)
+    border <- rep(border, length.out = nx)
+    lty <- rep(lty, length.out = nx)
+    angle <- rep(angle, length.out = nx)
+    density <- rep(density, length.out = nx)
+    for (i in 1:nx) {
+        n <- max(2, floor(edges * dx[i]))
+        t2p <- 2 * pi * seq(x[i], x[i + 1], length = n)
+        xc <- c(cos(t2p), 0) * radius+xpos
+        yc <- c(sin(t2p), 0) * radius+ypos
+        polygon(xc, yc, density = density[i], angle = angle[i],
+            border = border[i], col = col[i], lty = lty[i])
+    }
+    invisible(NULL)
+}
