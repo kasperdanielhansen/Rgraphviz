3afdf7a854e71ed578de4e8dbf526d804013374d
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2006-06-18 14:42:28 +0000

    fixed a bug in man page of image map lots of cosmetic improvements to vignette added imageMap demo to the vignette
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rgraphviz@18487 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/imageMap.R
+++ R/imageMap.R
@@ -36,13 +36,13 @@ setMethod("imageMap",
     stop("'height' must be numeric of length 1.")
   if( !is.numeric(usr) || length(usr)!=4 )
     stop("'usr' must be numeric of length 4.")
-
+  
   ## transform user to pixel coordinates
-  x.u2p = function(x) { (x-usr[1])/diff(usr[1:2])*width  }
-  y.u2p = function(y) { (usr[4]-y)/diff(usr[3:4])*height }
+  x.u2p = function(x) { rx=(x-usr[1])/diff(usr[1:2]); stopifnot(all(rx>=0&rx<=1)); return(rx*width)  }
+  y.u2p = function(y) { ry=(usr[4]-y)/diff(usr[3:4]); stopifnot(all(ry>=0&ry<=1)); return(ry*height) }
 
   nxy = getNodeXY(object)
-  nh  = getNodeHeight(object)
+  nh  = getNodeHeight(object)/2
   xl  =   floor(x.u2p( nxy$x - getNodeLW(object) ))
   xr  = ceiling(x.u2p( nxy$x + getNodeRW(object) ))
   yu  =   floor(y.u2p( nxy$y - nh ))
@@ -52,15 +52,16 @@ setMethod("imageMap",
   mapname <- paste("map", gsub(" |/|#|:", "_", imgname), sep="_")
   base::writeLines(paste("<IMG SRC=\"", imgname, "\" USEMAP=\#", mapname, " BORDER=0>",
                    "<MAP NAME=\"", mapname, "\">", sep=""), con)
-  for(nd in unique(unlist(sapply(tags, names)))) {
+  for(nd in unique(unlist(lapply(tags, names)))) {
     out = paste("<AREA SHAPE=\"rect\" COORDS=\"", xl[nd], ",", yl[nd], ",",
                 xr[nd], ",", yu[nd], "\"", sep="")
     for(i in seq(along=tags))
       if(nd %in% names(tags[[i]]))
         out = paste(out, " ", names(tags)[i], "=\"", tags[[i]][nd], "\"", sep="")
-    out = paste(out, "\">", sep="")
+    out = paste(out, ">", sep="")
     base::writeLines(out, con)
   }
+  base::writeLines("</MAP>", con)
 } ## end of definition
 ) ## end of setMethod
 
