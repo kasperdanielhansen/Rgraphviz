eafccce4ab5a7044824c18159c4807069b1e844c
Patrick Aboyoun <unknown>
2008-02-06 01:05:36 +0000

    Two fixes made: 1) Fixed a typo in a help file's \usage tag. 2) Added code to the .onLoad function to manage dll name conflicts with graph package.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rgraphviz@29846 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,23 @@
 .onLoad <- function(lib, pkg, where) {
-    library.dynam( "Rgraphviz", pkg, lib )
-    .Call("Rgraphviz_init", PACKAGE="Rgraphviz")
-    require("methods")
+	if(.Platform$OS.type == "windows") {
+		whichGvcDll <- unname(Sys.which("gvc.dll"))
+		if(nchar(whichGvcDll) >= 7) {
+			loadedDlls <- getLoadedDLLs()
+			graphPackagePosition <- which("graph" == names(loadedDlls))
+			graphPackageLoaded <- (length(graphPackagePosition) > 0)
+			if(graphPackageLoaded) {
+				graphDllPath <- loadedDlls[[graphPackagePosition]][["path"]]
+				graphLibraryPath <- dirname(dirname(graphDllPath))
+				graphDllName <- basename(graphDllPath)
+				library.dynam.unload(graphDllName, graphLibraryPath)
+			}
+			dyn.load(whichGvcDll)
+			if(graphPackageLoaded) {
+				library.dynam(graphDllName, "graph")
+			}
+		}
+	}
+	library.dynam( "Rgraphviz", pkg, lib )
+	.Call("Rgraphviz_init", PACKAGE="Rgraphviz")
+	require("methods")
 }
-
-
-
