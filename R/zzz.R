2f73201c9f856638d19ac44ac7d79b02490f9a23
Martin Morgan <mtmorgan@fhcrc.org>
2009-12-24 13:32:40 +0000

    .onLoad for Rgraphviz dll, to catch load errors more informatively
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rgraphviz@43763 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,27 @@
 .onLoad <- function(lib, pkg, where) {
+    tryCatch({
+        library.dynam('Rgraphviz', package='Rgraphviz')
+    }, error=function(err) {
+        msg <- sprintf("%s
+
+            Check that (1) graphviz is installed on your system; (2)
+            the installed version of graphviz matches '%s'; this is
+            the version used to build this Rgraphviz package; (3)
+            graphviz is accessible to R, e.g., the path to the
+            graphviz 'bin' directory is in the system 'PATH' variable.
+            See additional instructions in the 'README' file of the
+            Rgraphviz 'source' distribution, available at
+
+            http://bioconductor.org/packages/release/bioc/html/Rgraphviz.html
+
+            Ask further questions on the Bioconductor mailing list
+
+            http://bioconductor.org/docs/mailList.html
+
+            ", conditionMessage(err),
+               as.character(Rgraphviz:::.graphviz_build_version))
+        stop(paste(strwrap(msg, indent=4, exdent=4), collapse="\n"))
+    })
     .Call("Rgraphviz_init", PACKAGE = "Rgraphviz")
     versions <- graphvizVersion()
     if(versions$installed_version != versions$build_version) {
