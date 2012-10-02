9d236c89c1b8e88a280e3e12d8a39987ef2aee97
Kasper D. Hansen <khansen@stat.Berkeley.edu>
2012-10-02 03:56:48 +0000

    additional small fixes
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rgraphviz@70065 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -3,12 +3,12 @@
         library.dynam('Rgraphviz', pkgname, libname)
     }, error=function(err) {
         msg <- sprintf("
-            Check that (1) graphviz is installed on your system; (2)
-            the installed version of graphviz matches '%s'; this is
-            the version used to build this Rgraphviz package; (3)
-            graphviz is accessible to R, e.g., the path to the
-            graphviz 'bin' directory is in the system 'PATH' variable.
-            See additional instructions in the 'README' file of the
+            Rgraphviz is unable to be loaded.  This typically
+            is a symptom of an installation problem.  From 2.x.x
+            onwards, Graphviz ought to come bundled with Rgraphviz.
+
+            If you are trying to use an external Graphviz, see
+            additional instructions in the 'README' file of the
             Rgraphviz 'source' distribution, available at
 
             http://bioconductor.org/packages/release/bioc/html/Rgraphviz.html
@@ -21,13 +21,5 @@
         stop(conditionMessage(err), "\n\n",
              paste(strwrap(msg, indent=2, exdent=2), collapse="\n"))
     })
-    ## FIXME: might need to be uncommented for windows issue
     .Call("Rgraphviz_init", PACKAGE = "Rgraphviz")
-    ## versions <- graphvizVersion()
-    ## if(versions$installed_version != versions$build_version) {
-    ##     warning("Rgraphviz built with Graphviz version ",
-    ##             versions$build_version,
-    ##             ".\nFound installed Graphviz version ",
-    ##             versions$installed_version, ". This _may_ cause problems.")
-    ## }
 }
