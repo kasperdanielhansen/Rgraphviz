39e572b961ba1f328b2f6e9990d48bad495dcc8b
khansen <khansen@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-06-11 18:00:50 +0000

    Fixed an issue resulting in a warning message using pieGylph with default values (reported by Cristobal Fresno Rodríguez <Cristobalfresno@gmail.com>
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rgraphviz@77401 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotUtils.R
+++ R/plotUtils.R
@@ -1,6 +1,6 @@
 pieGlyph <- function (x, xpos, ypos, labels = names(x), edges = 200,
-    radius = 0.8, density = NULL, angle = 45, col = NULL, border =
-    NULL, lty = NULL, main = NULL, ...) {
+                      radius = 0.8, density = NULL, angle = 45, col = NULL, border =
+                      NULL, lty = NULL, main = NULL, ...) {
     if (!is.numeric(x) || any(is.na(x) | x <= 0))
         stop("pie: `x' values must be positive.")
     if (is.null(labels))
@@ -13,11 +13,16 @@ pieGlyph <- function (x, xpos, ypos, labels = names(x), edges = 200,
             c("lightblue", "mistyrose", "lightcyan",
               "lavender", "cornsilk", "white")
         else par("fg")
-    col <- rep(col, length.out = nx)
-    border <- rep(border, length.out = nx)
-    lty <- rep(lty, length.out = nx)
-    angle <- rep(angle, length.out = nx)
-    density <- rep(density, length.out = nx)
+    if(!is.null(col))
+        col <- rep(col, length.out = nx)
+    if(!is.null(border))
+        border <- rep(border, length.out = nx)
+    if(!is.null(lty))
+        lty <- rep(lty, length.out = nx)
+    if(!is.null(angle))
+        angle <- rep(angle, length.out = nx)
+    if(!is.null(density))
+        density <- rep(density, length.out = nx)
     for (i in 1:nx) {
         n <- max(2, floor(edges * dx[i]))
         t2p <- 2 * pi * seq(x[i], x[i + 1], length = n)
