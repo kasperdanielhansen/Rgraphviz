ae7af29f2bc35a0385ac1504dc42390bd0c10feb
Martin Morgan <mtmorgan.xyz@gmail.com>
2025-03-26 11:38:43 -0400

    free memory allocated in `graphvizCapabilities()`
    - `char**` return value needs to free'd as described in
      src/graphviz/lib/gvc/gvc.h
    - also fix leak in graphviz when duplicate entries are found
      src/graphviz/lib/gvc/gvcplugin.c

--- src/graphvizVersion.c
+++ src/graphvizVersion.c
@@ -20,46 +20,56 @@ SEXP Rgraphviz_capabilities(void) {
     PROTECT(capabilities_layout = NEW_CHARACTER(count));
     for (cc = 0; cc < count; cc++) {
 	SET_STRING_ELT(capabilities_layout, cc, mkChar(lp[cc]));
+        free(lp[cc]);
     }
     SET_VECTOR_ELT(capabilities, 0, capabilities_layout);
     SET_STRING_ELT(names, 0, mkChar("layoutTypes"));
     UNPROTECT(1);
+    free(lp);
     
     lp = gvPluginList(gvc, "render", &count, NULL);
     PROTECT(capabilities_render = NEW_CHARACTER(count));
     for (cc = 0; cc < count; cc++) {
 	SET_STRING_ELT(capabilities_render, cc, mkChar(lp[cc]));
+        free(lp[cc]);
     }
     SET_VECTOR_ELT(capabilities, 1, capabilities_render);
     SET_STRING_ELT(names, 1, mkChar("renderTypes"));
     UNPROTECT(1);
+    free(lp);
 
     lp = gvPluginList(gvc, "textlayout", &count, NULL);
     PROTECT(capabilities_textlayout = NEW_CHARACTER(count));
     for (cc = 0; cc < count; cc++) {
 	SET_STRING_ELT(capabilities_textlayout, cc, mkChar(lp[cc]));
+        free(lp[cc]);
     }
     SET_VECTOR_ELT(capabilities, 2, capabilities_textlayout);
     SET_STRING_ELT(names, 2, mkChar("textlayoutTypes"));
     UNPROTECT(1);
-    
+    free(lp);
+
     lp = gvPluginList(gvc, "device", &count, NULL);
     PROTECT(capabilities_device = NEW_CHARACTER(count));
     for (cc = 0; cc < count; cc++) {
 	SET_STRING_ELT(capabilities_device, cc, mkChar(lp[cc]));
+        free(lp[cc]);
     }
     SET_VECTOR_ELT(capabilities, 3, capabilities_device);
     SET_STRING_ELT(names, 3, mkChar("deviceTypes"));
     UNPROTECT(1);
+    free(lp);
 
     lp = gvPluginList(gvc, "loadimage", &count, NULL);
     PROTECT(capabilities_loadimage = NEW_CHARACTER(count));
     for (cc = 0; cc < count; cc++) {
 	SET_STRING_ELT(capabilities_loadimage, cc, mkChar(lp[cc]));
+        free(lp[cc]);
     }
     SET_VECTOR_ELT(capabilities, 4, capabilities_loadimage);
     SET_STRING_ELT(names, 4, mkChar("loadimageTypes"));
     UNPROTECT(1);
+    free(lp);
 
     setAttrib(capabilities, R_NamesSymbol, names);
     UNPROTECT(2);
